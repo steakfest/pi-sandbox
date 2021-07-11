8fb967ad8e4e9ae3424617b72f362d4953c05730
John McGowan <john@steakfest.com>
2021-07-11 14:27:53 -0600

    hello world
    Signed-off-by: John McGowan <john@steakfest.com>

---
+++ alpha-num-display-hello-world/hello-world.py
@@ -0,0 +1,14 @@
+import board
+from adafruit_ht16k33.segments import Seg14x4
+
+i2c = board.I2C()
+display = Seg14x4(i2c)
+
+display = Seg14x4(i2c, address=0x70)
+
+display.brightness = 0.5
+
+display.blink_rate = 3
+
+display.print(1234)
+
